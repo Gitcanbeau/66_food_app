40465e4792826acd7b5e8cf1a7556732660e0ddb
Gitcanbeau <canw2336@gmail.com>
2024-06-11 22:29:11 -0700

    Committing changes before updating dependencies

--- src/app/header/header.module.ts
+++ src/app/header/header.module.ts
@@ -1,5 +1,8 @@
 import { NgModule } from '@angular/core';
 import { CommonModule } from '@angular/common';
+import { MatTooltipModule } from '@angular/material/tooltip';
+import { MatIconModule } from '@angular/material/icon';
+import { MatFormFieldModule } from '@angular/material/form-field';
 
 import { HeaderRoutingModule } from './header-routing.module';
 import { HeaderComponent } from './components/header.component'; 
@@ -11,6 +14,9 @@ import { HeaderComponent } from './components/header.component';
   ],
   imports: [
     CommonModule,
+    MatTooltipModule,
+    MatIconModule,
+    MatFormFieldModule,
     HeaderRoutingModule
   ],
   exports:[HeaderComponent]
